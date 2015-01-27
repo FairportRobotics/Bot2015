c086772130b806ba768d41daacf2fdfba9588547
abush3 <alexbush410@gmail.com>
2015-01-27 18:31:24 -0500

    Eject Totes command finished?

--- src/org/usfirst/frc/team578/robot/commands/EjectTotesCommand.java
+++ src/org/usfirst/frc/team578/robot/commands/EjectTotesCommand.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team578.robot.commands;
 
 import org.usfirst.frc.team578.robot.Robot;
-import org.usfirst.frc.team578.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
